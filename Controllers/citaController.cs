5894591c0b1e6f8dcff3fde38ff99977524f1e04
Eduardo Raul Rojas Huaman <nitratospeed@gmail.com>
2020-01-14 18:26:13 -0500

    new cita

---
+++ Controllers/citaController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using citas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace citas.Controllers
+{
+    public class citaController : Controller
+    {
+        private readonly CitasContext _context;
+
+        public citaController(CitasContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
